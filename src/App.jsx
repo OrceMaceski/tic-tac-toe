56b0e65dda65f4abd9c8e4d8f56b71b143e1f11a
orcemaceski <maceskiorce@gmail.com>
2024-07-01 16:26:42 +0200

    Set active player and change symbols

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
+import { useState } from "react";
 import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
 import "./index.css";
 
 function App() {
+  const [activePlayer, setActivePlayer] = useState("X");
+
+  const handleSelectSquare = () => {
+    setActivePlayer((prevPlayer) => (prevPlayer === "X" ? "O" : "X"));
+  };
+
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-          <Player name="Player 1" symbol="X" />
-          <Player name="Player 2" symbol="O" />
+        <ol id="players" className="highlight-player">
+          <Player name="Player 1" symbol="X" isActive={activePlayer === "X"} />
+          <Player name="Player 2" symbol="O" isActive={activePlayer === "O"} />
         </ol>
-        <GameBoard />
+        <GameBoard
+          onSelectSquare={handleSelectSquare}
+          activePlayerSymbol={activePlayer}
+        />
       </div>
+      <div>LOG</div>
     </main>
   );
 }
