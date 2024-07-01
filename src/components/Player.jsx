1c1736b6d87ac078834d1f4003676cbb23cd8ba6
orcemaceski <maceskiorce@gmail.com>
2024-07-01 15:56:57 +0200

    IMplement players

---
+++ src/components/Player.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+export default function Player({ name, symbol }) {
+  const [playerName, setPlayerName] = useState(name);
+  const [isEditing, setIsEditing] = useState(false);
+
+  const handleEdit = () => {
+    setIsEditing((editing) => !editing); // !!!! instead of !isEditing
+  };
+
+  const handleChange = (event) => {
+    setPlayerName(event.target.value);
+  };
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+
+  if (isEditing) {
+    editablePlayerName = (
+      <input
+        type="text"
+        required
+        value={playerName}
+        onChange={(args) => handleChange(args)}
+      />
+    );
+  }
+
+  return (
+    <li>
+      <span className="player">
+        {editablePlayerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEdit}>{!isEditing ? "EDIT" : "SAVE"}</button>
+    </li>
+  );
+}
