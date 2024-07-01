1cd19cca2fdc753f0acc906f5cee28cfa8aa6911
orcemaceski <maceskiorce@gmail.com>
2024-07-01 20:39:27 +0200

    Refactor code to remove state

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,30 @@ import GameBoard from "./components/GameBoard";
 import Log from "./components/Log";
 import "./index.css";
 
+function deriveActivePlayer(gameTurns) {
+  let currentPlayer = "X";
+  if (gameTurns.length > 0 && gameTurns[0].player === "X") {
+    currentPlayer = "O";
+  }
+
+  return currentPlayer;
+}
+
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
-  const [activePlayer, setActivePlayer] = useState("X");
+  // const [activePlayer, setActivePlayer] = useState("X");
+
+  const activePlayer = deriveActivePlayer(gameTurns);
 
   const handleSelectSquare = (rowIndex, cellIndex) => {
-    setActivePlayer((prevPlayer) => (prevPlayer === "X" ? "O" : "X"));
+    // setActivePlayer((prevPlayer) => (prevPlayer === "X" ? "O" : "X"));
     setGameTurns((prevTurns) => {
-      let currentPlayer = "X";
-      if (prevTurns.length > 0 && prevTurns[0].player === "X") {
-        currentPlayer = "O";
-      }
+      const currentPlayer = deriveActivePlayer(prevTurns);
+
+      // let currentPlayer = "X";
+      // if (prevTurns.length > 0 && prevTurns[0].player === "X") {
+      //   currentPlayer = "O";
+      // }
 
       const updatedTurns = [
         { square: { row: rowIndex, cell: cellIndex }, player: currentPlayer },
