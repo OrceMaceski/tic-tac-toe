ced6ac70c58da2a7f70b2525bf3a738b6921ea70
orcemaceski <maceskiorce@gmail.com>
2024-07-01 15:57:32 +0200

    Implement initial GameBoard

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
 
-function GameBoard() {
-  return <div>GameBoard</div>;
+export default function GameBoard() {
+  return (
+    <ol id="game-board">
+      {initialGameBoard.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, cellIndex) => (
+              <li key={cellIndex}>
+                <button>{playerSymbol}</button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
 }
-
-export default GameBoard;
